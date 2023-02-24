f88e0c79f5fc43dd9b2a74b95d9c19704f6123dd
Miguel Soto <miguelsoto@Miguels-MacBook-Pro.local>
2023-02-24 16:25:48 -0600

    initial commit, basic project structre, app.py Dockerfile and dependencies

---
+++ eCommBackEnd/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flaskext.mysql import MySQL
+# import * from ./services/App.py
+
+mysql = MySQL()
+
+app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER'] = ''
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = ''
+app.config['MYSQL_DATABASE_HOST'] = ''
+
+mysql.init_app(app)
+
+
+'''
+Methods for now
+'''
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+if __name__ == '__main__':
+    app.run(debug=True)
