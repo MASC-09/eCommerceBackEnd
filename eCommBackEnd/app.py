f38ca62c1e76175aadca30b7cde6e7831ae09c77
Miguel Soto <miguelsoto@Miguels-MacBook-Pro.local>
2023-03-01 15:27:55 -0600

    added the basic database configuration, sqlite, the script for the database, and the db is created now

--- eCommBackEnd/app.py
+++ eCommBackEnd/app.py
@@ -1,25 +1,25 @@
-from flask import Flask
-from flaskext.mysql import MySQL
-# import * from ./services/App.py
+# from flask import Flask
+# from flaskext.mysql import MySQL
+# # import * from ./services/App.py
 
-mysql = MySQL()
+# mysql = MySQL()
 
-app = Flask(__name__)
-app.config['MYSQL_DATABASE_USER'] = ''
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = ''
-app.config['MYSQL_DATABASE_HOST'] = ''
+# app = Flask(__name__)
+# app.config['MYSQL_DATABASE_USER'] = ''
+# app.config['MYSQL_DATABASE_PASSWORD'] = ''
+# app.config['MYSQL_DATABASE_DB'] = ''
+# app.config['MYSQL_DATABASE_HOST'] = ''
 
-mysql.init_app(app)
+# mysql.init_app(app)
 
 
-'''
-Methods for now
-'''
+# '''
+# Methods for now
+# '''
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
